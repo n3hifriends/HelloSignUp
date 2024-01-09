a1da83387131a92f55b1befc1fc4da9c52d644a4
Nitin Kalokhe <nitin.kalokhe@godigit.com>
2024-01-09 22:40:23 +0530

    feat: basic app flow is working

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -11,7 +11,7 @@ i18n.fallbacks = true
  * the language code is the suffixed with "-US". i.e. if a device is set to English ("en"),
  * if you change to another language and then return to English language code is now "en-US".
  */
-i18n.translations = { ar, en, "en-US": en, ko, fr }
+i18n.translations = { en, "en-US": en }
 
 const locales = Localization.getLocales() // This method is guaranteed to return at least one array item.
 // The preferred language is the first element in the array, however, we fallback to en-US, especially for tests.
